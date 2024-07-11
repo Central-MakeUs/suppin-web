ae65fafc4b1053c47c8af363655619b8bde6cac3
JunYoungKr <kop981020@naver.com>
2024-07-11 18:51:18 +0900

    fix: [4] splashscreen 삭제

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,27 +1,14 @@
-import { useState, useEffect } from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from '@/pages';
-
 import GlobalStyles from './styles/global-styles';
-import SplashScreen from '@/components/SplashScreen';
 
 const router = createBrowserRouter([{ path: '/', element: <HomePage /> }]);
 
 export default function App() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 3000); // 3초 후에 로딩 종료
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <>
       <GlobalStyles />
-      {isLoading ? <SplashScreen /> : <RouterProvider router={router} />}
+      <RouterProvider router={router} />
     </>
   );
 }
