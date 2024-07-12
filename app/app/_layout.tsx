6c7e9061f4d0d588f773cc5905300794bee488c6
김영훈 <joseph0926@naver.com>
2024-07-12 12:56:38 +0900

    chore: [5] 앱 기본 초기 세팅

---
+++ app/app/_layout.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const MainLayout = () => {
+  return (
+    <View>
+      <Text>MainLayout</Text>
+    </View>
+  );
+};
+
+export default MainLayout;
+
+const styles = StyleSheet.create({});
