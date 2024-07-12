697ea63e4f07a9ea38a922144088e9c77c3984fe
김영훈 <joseph0926@naver.com>
2024-07-12 17:08:42 +0900

    feat: [7] 스토리북 추가

---
+++ web/.storybook/preview.ts
@@ -0,0 +1,24 @@
+import type { Preview } from '@storybook/react';
+import { withThemeByDataAttribute } from '@storybook/addon-themes';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    withThemeByDataAttribute({
+      defaultTheme: 'light',
+      themes: {
+        light: 'light',
+        dark: 'dark',
+      },
+    }),
+  ],
+};
+
+export default preview;
