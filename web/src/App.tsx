4b3f6ca51345abfb6268eb60bc2dbb53698164b4
joseph0926 <joseph0926@naver.com>
2024-07-14 10:29:06 +0900

    feat: [4] react-router-dom을 이용한 페이지 구조 잡기

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,32 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { HomePage } from '@/pages';
-import GlobalStyles from './styles/global-styles';
+import {
+  AuthPage,
+  CollectCommentsPage,
+  CreateSurveyPage,
+  HomePage,
+  ResultPage,
+  Root,
+} from '@/pages';
+import store from '@/store';
+import GlobalStyles from '@/styles/global-styles';
 import { Provider } from 'react-redux';
-import store from './store';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const router = createBrowserRouter([{ path: '/', element: <HomePage /> }]);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: 'result', element: <ResultPage /> },
+      {
+        path: 'survey',
+        children: [{ path: 'new', element: <CreateSurveyPage /> }],
+      },
+      { path: 'collect', element: <CollectCommentsPage /> },
+    ],
+  },
+  { path: '/auth', element: <AuthPage /> },
+]);
 
 export default function App() {
   return (
