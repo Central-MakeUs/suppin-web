99bda5e553b1958871fa0a97a5e4d7ae27e74bc8
joseph0926 <joseph0926@naver.com>
2024-07-20 11:22:05 +0900

    feat: [30] App 스플래시 화면 구축

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,11 +1,40 @@
 import { Stack } from "expo-router";
-import React from "react";
-import { StyleSheet } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import React, { useCallback, useEffect, useState } from "react";
+import { View } from "react-native";
 
-const MainLayout = () => {
-  return <Stack />;
-};
+SplashScreen.preventAutoHideAsync();
 
-export default MainLayout;
+export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
 
-const styles = StyleSheet.create({});
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <Stack />
+    </View>
+  );
+}
