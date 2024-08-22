acd991d39dccfb1bcc8b6506d55a4e516e6b1253
김영훈 <joseph0926@naver.com>
2024-08-22 15:16:08 +0900

    fix: 안드로이드 뒤로가기 대응

--- app/components/webview-bridge.tsx
+++ app/components/webview-bridge.tsx
@@ -1,14 +1,31 @@
 import { useWebViewBridge } from "@/hooks/use-webview";
 import { INJECTED_JAVASCRIPT } from "@/lib/log";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { BackHandler } from "react-native";
 import WebView from "react-native-webview";
 
 interface WebViewBridgeProps {
   source: { uri: string };
 }
 
-export const WebViewBridge: React.FC<WebViewBridgeProps> = ({ source }) => {
+export const WebViewBridge = ({ source }: WebViewBridgeProps) => {
   const { webviewRef, onMessage } = useWebViewBridge();
+  const [canGoBack, setCanGoBack] = useState(false);
+
+  useEffect(() => {
+    const onBackPress = () => {
+      if (canGoBack && webviewRef.current) {
+        webviewRef.current.goBack();
+        return true;
+      }
+      return false;
+    };
+
+    BackHandler.addEventListener("hardwareBackPress", onBackPress);
+
+    return () =>
+      BackHandler.removeEventListener("hardwareBackPress", onBackPress);
+  }, [canGoBack]);
 
   return (
     <WebView
@@ -16,6 +33,9 @@ export const WebViewBridge: React.FC<WebViewBridgeProps> = ({ source }) => {
       source={source}
       onMessage={onMessage}
       injectedJavaScript={INJECTED_JAVASCRIPT}
+      onNavigationStateChange={(navState) => {
+        setCanGoBack(navState.canGoBack);
+      }}
     />
   );
 };
